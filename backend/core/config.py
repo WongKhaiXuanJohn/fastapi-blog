a354d59e9696626dcc074c4b78b4cd48ab578f97
WongKhaiXuanJohn <john_john@live.com.my>
2024-09-02 14:28:41 +0800

    setup postgres connection and credentials

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,5 +1,28 @@
+import os
+from dotenv import load_dotenv
+
+from pathlib import Path
+env_path = Path(__file__).resolve().parent.parent / '.env'
+load_dotenv(dotenv_path=env_path)
+"""
+Path(__file__) gets the path of the current file, config.py.
+.resolve() gets the absolute path.
+.parent.parent navigates two levels up from the core directory to the backend directory, where the .env file is located.
+/ '.env' appends the .env filename to the path.
+"""
+
 class Settings:
     PROJECT_TITLE: str = "Blog 👋"
     PROJECT_VERSION: str = "0.1.0"
+
+    POSTGRES_USER: str = os.getenv("POSTGRES_USER")
+    POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD")
+    POSTGRES_SERVER: str = os.getenv("POSTGRES_SERVER", "localhost")
+    POSTGRES_PORT: int = os.getenv("POSTGRES_PORT", 5432)
+    POSTGRES_DB: str = os.getenv("POSTGRES_DB")
+    POSTGRES_USER: str = os.getenv("POSTGRES_USER")
+    DATABASE_URL: str = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
+
+
 
 settings = Settings()
